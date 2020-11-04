522eba1b5bf00b2a6ff6e11243e7658894348385
Chuma Takahiro <chuma@unicorn.limited>
2020-11-04 18:37:37 +0900

    add revenue share contract generation

--- lucadeal/lib/luca_deal/contract.rb
+++ lucadeal/lib/luca_deal/contract.rb
@@ -26,22 +26,45 @@ module LucaDeal
       end
     end
 
-    def generate!(customer_id)
+    def generate!(customer_id, mode = nil)
       LucaDeal::Customer.find(customer_id) do |customer|
         current_customer = parse_current(customer)
         obj = { 'customer_id' => current_customer['id'], 'customer_name' => current_customer['name'] }
-        obj['items'] = [{
-                          'name' => '_ITEM_NAME_FOR_INVOICE_',
-                          'qty' => 1,
-                          'price' => 0
-                        }]
-        obj['terms'] = { 'billing_cycle' => 'monthly', 'effective' => @date }
+        obj['terms'] = { 'effective' => @date }
+        if mode == 'sales_fee'
+          obj.merge! salesfee_template
+        else
+          obj.merge! monthly_template
+        end
         self.class.create(obj)
       end
     end
 
     def self.active_period?(dat)
       !dat.dig('terms').nil?
+    end
+
+    private
+
+    def monthly_template
+      {}.tap do |obj|
+        obj['items'] = [{
+                          'name' => '_ITEM_NAME_FOR_INVOICE_',
+                          'qty' => 1,
+                          'price' => 0
+                        }]
+        obj['terms'] = { 'billing_cycle' => 'monthly' }
+      end
+    end
+
+    def salesfee_template
+      {}.tap do |obj|
+        obj['rate'] = {
+          'default' => '0.2',
+          'initial' => '0.2'
+        }
+        obj['terms'] = { 'category' => 'sales_fee' }
+      end
     end
   end
 end
