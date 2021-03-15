084924d5d334430195675b7784de61988a5953f8
Chuma Takahiro <chuma@unicorn.limited>
2021-03-15 15:24:14 +0900

    Fix: version

--- lucabook/lib/luca_book/version.rb
+++ lucabook/lib/luca_book/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LucaBook
-  VERSION = '0.2.27'
+  VERSION = '0.2.26'
 end
--- lucarecord/lib/luca_record/version.rb
+++ lucarecord/lib/luca_record/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LucaRecord
-  VERSION = '0.2.26'
+  VERSION = '0.2.27'
 end
